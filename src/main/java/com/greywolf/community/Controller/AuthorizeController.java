b661373c18f743d1bb5da3542f83382c2789b97d
GreyWolf999 <1139697779@qq.com>
2020-02-24 21:58:44 +0800

    secondCommit

---
+++ src/main/java/com/greywolf/community/Controller/AuthorizeController.java
@@ -0,0 +1,46 @@
+package com.greywolf.community.Controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.greywolf.community.dbo.AccessTokenDTO;
+import com.greywolf.community.dbo.GitHubUser;
+import com.greywolf.community.provide.AccessTokenProvide;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+    @Value("client_id")
+    private String client_id;
+    @Value("client_secret")
+    private String client_secret;
+    @Value("redirect_uri")
+    private String redirect_uri;
+
+    @Autowired
+    AccessTokenProvide accessTokenProvide;
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code,
+                           @RequestParam("state") String state,
+                           Model model) throws JsonProcessingException {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_secret(redirect_uri);
+        accessTokenDTO.setState(state);
+        GitHubUser gitHubUser = accessTokenProvide.gitHubUser(accessTokenProvide.getAccessToken(accessTokenDTO));
+        model.addAttribute("GitHubUser",gitHubUser);
+        return "index";
+    }
+
+
+}
---
+++ src/main/java/com/greywolf/community/provide/AccessTokenProvide.java
@@ -0,0 +1,49 @@
+package com.greywolf.community.provide;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greywolf.community.dbo.AccessTokenDTO;
+import com.greywolf.community.dbo.GitHubUser;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AccessTokenProvide {
+    private GitHubUser gitHubUser;
+    private String AccessToken;
+    @Autowired
+    ObjectMapper objectMapper;
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) throws JsonProcessingException {
+        MediaType JSON
+                = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+            RequestBody body = RequestBody.create(JSON,objectMapper.writeValueAsString(accessTokenDTO));
+            Request request = new Request.Builder()
+                    .url("https://github.com/login/oauth/access_token")
+                    .post(body)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                AccessToken= response.body().string().split("&")[0].split("=")[1];
+                System.out.println(AccessToken);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return AccessToken;
+    }
+    public GitHubUser gitHubUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try (Response ignored = client.newCall(request).execute()) {
+            gitHubUser = objectMapper.readValue(ignored.body().string(), GitHubUser.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            return gitHubUser;
+    }
+}
+
