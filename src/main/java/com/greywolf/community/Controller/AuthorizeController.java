a27807dc124544f81826b2b029a94a698ce144ee
GreyWolf999 <1139697779@qq.com>
2020-02-24 22:27:23 +0800

    commit

--- src/main/java/com/greywolf/community/Controller/AuthorizeController.java
+++ src/main/java/com/greywolf/community/Controller/AuthorizeController.java
@@ -4,17 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.greywolf.community.dbo.AccessTokenDTO;
 import com.greywolf.community.dbo.GitHubUser;
 import com.greywolf.community.provide.AccessTokenProvide;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.IOException;
 
 @Controller
 public class AuthorizeController {
